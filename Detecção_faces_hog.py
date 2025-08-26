4a69e3919386001758f421c40c9729f153abd093
Kaio Martinhago <kaiofmartinhago@gmail.com>
2025-08-26 17:04:17 -0300

    Add files via upload

---
+++ Detecção_faces_hog.py
@@ -0,0 +1,14 @@
+import dlib
+import cv2
+
+imagem = cv2.imread('Visao_Computacional_Guia_Completo\Images\people2.jpg')
+detector_faces = dlib.get_frontal_face_detector()
+
+deteccoes = detector_faces(imagem, 2)
+
+for face in deteccoes:
+    l,t,r,b = face.left(), face.top(), face.right(), face.bottom()
+    cv2.rectangle(imagem, (l,t), (r,b), (0,0,255), 2)
+cv2.imshow('Imagem_hog', imagem)
+cv2.waitKey(0)
+cv2.destroyWindow('Imagem_hog')
