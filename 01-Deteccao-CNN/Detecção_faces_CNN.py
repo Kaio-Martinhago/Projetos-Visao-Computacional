61e3ffa63206f97583090527557190cf175bb6ee
Kaio Martinhago <kaiofmartinhago@gmail.com>
2025-08-26 17:12:25 -0300

    Update Detecção_faces_CNN.py

--- 01-Deteccao-CNN/Detecção_faces_CNN.py
+++ 01-Deteccao-CNN/Detecção_faces_CNN.py
@@ -1,1 +1,15 @@
+import cv2
+import dlib
 
+imagem = cv2.imread('Visao_Computacional_Guia_Completo\Images\people2.jpg')
+
+detector_face_cnn = dlib.cnn_face_detection_model_v1('Visao_Computacional_Guia_Completo\Weights\mmod_human_face_detector.dat')
+
+deteccoes = detector_face_cnn(imagem, 1)
+for face in deteccoes:
+    l,t,r,b,c = face.rect.left(), face.rect.top(), face.rect.right(), face.rect.bottom(), face.confidence
+    print(c)
+    cv2.rectangle(imagem, (l,t), (r,b), (255,255,0),2)
+cv2.imshow('Imagem_cnn', imagem)
+cv2.waitKey(0)
+cv2.destroyWindow('Imagem_cnn')
